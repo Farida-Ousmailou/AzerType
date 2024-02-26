46a3a02d13ca29d24951ec29c4e19e46d1b9e6d5
Atto Deba Souleymane <AttoDeba1@users.noreply.github.com>
2024-02-26 21:34:57 +0100

    code for  initialization

---
+++ script.js
@@ -0,0 +1,7 @@
+const motApplication = "Bonjour"
+let motUtilisateur = prompt("Entrez le mot: " + motApplication)
+if (motUtilisateur === motApplication) {
+    console.log("Bravo, vous bien saisi le mot")
+} else { 
+    console.log("Désolé, vous avez fait une erreur")
+}
