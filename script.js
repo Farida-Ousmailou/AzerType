714990c65284c14d4d2274f2d6a59882b1dd3911
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-27 22:24:24 +0100

    refactoring with fonction

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const listeMots= ["Cachalot", "Incroyable", "institution"]
 const listePhrases= ["Le cachalot est un mammifère marin", "C'est incroyable ce que tu as fait", "L'institution est un lieu de savoir"]
 
-let score = 0
-let choix = prompt("Voulez-vous jouer avec des mots ou des phrases?")
-while (choix !== "mots" && choix !== "phrases") {
-    choix = prompt("Veuillez choisir entre les mots et les phrases")
+
+function afficherResultat(score, nbMotsProposes){
+    return console.log("Votre score est de: " + score + "sur" + nbMotsProposes.length)
 }
-if (choix === "mots") {
-    for (let i = 0; i < listeMots.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
-        if (motUtilisateur === listeMots[i]) {
-            console.log("Bravo, vous bien saisi le mot")
-            score++
-        }
+function choisirPhraseOuMots(){
+    let choix = prompt("Voulez-vous jouer avec des mots ou des phrases?")
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt("Veuillez choisir entre les mots et les phrases")
     }
-    console.log("Votre score est de: " + score + "sur" + listeMots.length)
-} else {
-    for(let i=0; i <listePhrases.length; i++){
-        let phraseUtilisateur = prompt("Entrez la phrase: " + listePhrases[i])
-        if(phraseUtilisateur === listePhrases[i]){
-            score++
+    return choix
+}
+function lancerBoucleDeJeu(listePropositions){
+    let score = 0
+    if (choix === "mots") {
+        for (let i = 0; i < listeMots.length; i++) {
+            let motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
+            if (motUtilisateur === listeMots[i]) {
+                console.log("Bravo, vous bien saisi le mot")
+                score++
+            }
         }
+        return score
+    } else {
+        for(let i=0; i <listePhrases.length; i++){
+            let phraseUtilisateur = prompt("Entrez la phrase: " + listePhrases[i])
+            if(phraseUtilisateur === listePhrases[i]){
+                score++
+            }
+        }
+        return score
     }
-    console.log("Votre score est de: " + score + "sur" + listePhrases.length)
 }
