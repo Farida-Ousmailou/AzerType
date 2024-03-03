7280ba1572fdf7220344a52ad2f6dd8cc7c18651
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-03-03 22:23:38 +0100

    Add event listener for input submission in lancerJeu() for improved user interaction.

--- script.js
+++ script.js
@@ -10,45 +10,21 @@ function afficherResultat(score, nbMotsProposes){
     return console.log("Votre score est de: " + score + "sur" + nbMotsProposes.length)
 }
 
-function choisirPhraseOuMots(){
-    let choix = prompt("Voulez-vous jouer avec des mots ou des phrases?")
-    while (choix !== "mots" && choix !== "phrases") {
-        choix = prompt("Veuillez choisir entre les mots et les phrases")
-    }
-    return choix
-}
-function lancerBoucleDeJeu(listePropositions){
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
-        if (motUtilisateur === listePropositions[i]) {
-                score++
-         }
-     }
-        return score    
- }
-
 function lancerJeu (){
-    let choix = choisirPhraseOuMots()
     let score = 0
     let nbMotsProposes = 0
-    
-    if (choix === "mots") {
-        lancerBoucleDeJeu(listeMots)
-        nbMotsProposes = listeMots.length
-    } else {
-        lancerBoucleDeJeu(listePhrases)
-        nbMotsProposes = listePhrases.length
-    }
+    let inputtext= document.getElementById("texte")
+    let buttonValider = document.getElementById("bouton")
+    buttonValider.addEventListener("click", function(){
+        console.log(inputtext.value)
+    })
+
     afficherResultat(score, nbMotsProposes)
 }
 
 lancerJeu()
 
-let inputtext= document.getElementById("texte")
-let buttonValider = document.getElementById("bouton")
-console.log(inputtext)
-console.log(buttonValider)
+
 
 let textErea= document.querySelector(".textErea")
 console.log(textErea)
