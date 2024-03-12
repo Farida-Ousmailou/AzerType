da0ad3a9e196ae0ba6410af19f89c879d96656ea
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-03-12 23:55:43 +0100

    add a function

--- partager.js
+++ partager.js
@@ -2,3 +2,9 @@ function afficherPartage() {
     let partageScore = document.querySelector(".classScore")
     partageScore.classList.add("active")
 }
+
+function cacherPartage() {
+    let popupBackground = document.querySelector(".classScore") 
+    partageScore.classList.remove("active")
+}
+afficherPartage
